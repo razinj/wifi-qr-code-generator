a850f098f6a7e02c75dcd4fc7baa1dc9121bc0a9
razinj <nizarjailane@gmail.com>
2022-08-06 22:39:23 +0100

    Make test clear with optional param

--- src/utils/string.test.ts
+++ src/utils/string.test.ts
@@ -2,7 +2,7 @@ import { escapeChars } from './string'
 
 describe('String Utils', () => {
   it('should escape illegal characters supplied', () => {
-    const result = escapeChars('A value with characters to be escaped - , ; : " \\')
+    const result = escapeChars('A value with characters to be escaped - , ; : " \\', [',', ':', ';', '"', '\\'])
 
     expect(result).toBe('A value with characters to be escaped - \\, \\; \\: \\" \\\\')
   })
