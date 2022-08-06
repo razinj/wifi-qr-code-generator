eb0505f323f139ce3a3fba27aa885a4891ea0942
razinj <nizarjailane@gmail.com>
2022-08-06 22:16:49 +0100

    Create string.test.ts

---
+++ src/utils/string.test.ts
@@ -0,0 +1,9 @@
+import { escapeChars } from './string'
+
+describe('String Utils', () => {
+  it('should escape illegal characters supplied', () => {
+    const result = escapeChars('A value with characters to be escaped - , ; : " \\')
+
+    expect(result).toBe('A value with characters to be escaped - \\, \\; \\: \\" \\\\')
+  })
+})
